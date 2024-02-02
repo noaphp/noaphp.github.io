4062db2650eb1354a2c486bb757a2c4424d9c7f1
Abubakar Ibrhim Umar <abubakaribrahim30012@gmail.com>
2024-02-02 21:37:36 +0100

    welcome page for the framework

---
+++ welcome.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Noaphp</title>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="./asset//mystyle/welcome.css">
+    <link rel="stylesheet" href="./asset/fontawesome-free/css/all.min.css">
+</head>
+
+<body class="antialiased">
+    <div class="container">
+        <div class="welcome-logo">
+            <img src="./asset/photos/nowerphplogo.png" alt="">
+        </div>
+        <div class="welcome-grid">
+            <a href="https://noaphp.github.io" target="_blank" >
+                <div class="col-6">
+                    <div class="card">
+                        <div class="card-icon">
+                            <i class="fa fa-book-open"></i>
+                        </div>
+                        <div class="card-text">
+                            <div class="justify-space">
+                                <div class="content-title">
+                                    <h4>Documentation</h4>
+                                    <p>Noaphp has wonderful documentation covering every aspect of the framework. Whether you are a newcomer or have prior experience with Noaphp, we recommend reading our documentation from beginning to end.</p>
+                                </div>
+                                <div class="content">
+                                    <i class="fa fa-arrow-right text-defult"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+            <a href="">
+                <div class="col-6">
+                    <div class="card">
+                        <div class="card-icon">
+                            <i class="fa fa-video"></i>
+                        </div>
+                        <div class="card-text">
+                            <div class="justify-space">
+                                <div class="content-title">
+                                    <h4>Noacasts</h4>
+                                    <p>Noacasts offers thousands of video tutorials on Noaphp, PHP, and JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.</p>
+                                </div>
+                                <div class="content">
+                                    <i class="fa fa-arrow-right text-defult"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+            <a href="">
+                <div class="col-6">
+                    <div class="card">
+                        <div class="card-icon">
+                            <i class="fa fa-newspaper"></i>
+                        </div>
+                        <div class="card-text">
+                            <div class="justify-space">
+                                <div class="content-title">
+                                    <h4>Noaphp News</h4>
+                                    <p>Noaphp News is a community driven portal and newsletter aggregating all of the latest and most important news in the Noaphp ecosystem, including new package releases and tutorials.</p>
+                                </div>
+                                <div class="content">
+                                    <i class="fa fa-arrow-right text-defult"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+            
+            <div class="col-6">
+                <div class="card">
+                    <div class="card-icon">
+                        <i class="fa fa-globe-africa"></i>
+                    </div>
+                    <div class="card-text">
+                        <h4>Vibrant Ecosystem</h4>
+                        <p>Noephp's robust library of first-party tools and libraries, such as Forge, Vapor, Nova, and Envoyer help you take your projects to the next level. Pair them with powerful open source libraries like Cashier, Dusk, Echo, Horizon, Sanctum, Telescope, and more.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="width-90">
+            <div class="noaphp-sponse">
+                <a href="">
+                    <i class="fa fa-regular fa-heart"></i>
+                    <p>Sponsor</p>
+                </a>
+            </div>
+            <div class="vertion">
+                <p>Noaphp v1.0.0 (PHP v <?=PHP_VERSION?>)</p>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
